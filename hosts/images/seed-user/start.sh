ba2514d0228b4baf8a6536c054bc1716cd967d3e
Harish Navnit <harishnavnit_rajasekaran@mymail.sutd.edu.sg>
2023-12-18 05:02:49 -0500

    reorganise directory structure

--- hosts/images/seed-user/start.sh
+++ hosts/images/seed-user/start.sh
@@ -3,6 +3,8 @@
 # Copy the prepared resolv.conf file
 cp /etc/resolv.conf.override /etc/resolv.conf
 
+echo "export PS1='$(whoami)@user: [$(pwd)] $ '" >> $HOME/.bashrc
+
 # Start the shell
 /bin/bash
 
